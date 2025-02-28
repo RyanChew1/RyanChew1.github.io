1d59688d1ebd5b0edba647646c26546747c6bb43
“RyanChew1” <chewryan0@gmail.com>
2025-02-27 17:07:18 -0800

    fix type error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-    <link 
+import type { AppProps } from 'next/app'
+<link 
   rel="stylesheet" 
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" 
 />
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
